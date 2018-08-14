002301ec86b13ea00b1f2516f8b465a16cc0d4bb
Ekaterina Khotko <ekhotko@plgn.ru>
2018-08-14 15:32:21 +0500

    First pyrogram command

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from pyrogram import Client, Filters
 app = Client("662893418:AAEMfj8jRYHlYZTTOa4yqzZp9uEDdyyTMDI")
 
 
-@app.on_message(Filters.text & Filters.private)
-def echo(client, message):
+@app.on_message(Filters.command(["start", "help"]))
+def hello(client, message):
     client.send_message(
-        message.chat.id, message.text,
-        reply_to_message_id=message.message_id
-    )
+        message.chat.id,
+        '''
+        Hello! I can just teach you how to play a hero
+        sending you some talents.
+        ''')
 
 
 app.run()
