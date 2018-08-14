4e23086912273e0ae5638b3478f62bea5ffea49b
Ekaterina Khotko <ekhotko@plgn.ru>
2018-08-14 15:35:30 +0500

    Fix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Client("662893418:AAEMfj8jRYHlYZTTOa4yqzZp9uEDdyyTMDI")
 @app.on_message(Filters.command(["start", "help"]))
 def hello(client, message):
     client.send_message(
-        message.chat.id, message.text,
+        message.chat.id,
         '''
         Hello! I can just teach you how to play a hero
         sending you some talents.
