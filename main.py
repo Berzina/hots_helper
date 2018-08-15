16c742c2d245bdae1b7c7aa403e02f55bbec1906
Ekaterina Khotko <ekhotko@plgn.ru>
2018-08-15 13:44:21 +0500

    Fix lol

--- main.py
+++ main.py
@@ -20,16 +20,19 @@ sending you some talents.''')
 
 
 @app.on_message(Filters.command(["test"]))
+def test_blizz(client, message):
+    some_hero = fetcher.HAPPY_HEROES.take_by_name('Артас')[0]
+    page = fetcher.fetch_blizz(some_hero.build_refs[0].link)
+
+    print(page)
+
+
+@app.on_message(Filters.command(["test"]))
 def hero_list(client, message):
-    some_hero = HAPPY_HEROES.take_by_name('Артас')[0]
-    page = fetch_blizz(some_hero.build_refs[0].link)
-
-    bp = BlizzParser(page)
-
-    # client.send_message(
-    #     message.chat.id,
-    #     fetcher.HAPPY_HEROES.prepare_build_response(message.text),
-    # )
+    client.send_message(
+        message.chat.id,
+        fetcher.HAPPY_HEROES.prepare_build_response(message.text)
+    )
 
 
 app.run()
