7561f4a87a444d50f045d6a3c950970a03900f32
Ekaterina Khotko <ekhotko@plgn.ru>
2018-08-17 13:51:27 +0500

    Still trying

--- callbacks.py
+++ callbacks.py
@@ -3,8 +3,9 @@ from utils.views import responce_form
 
 
 def echo(user_id, message):
+    print(user_id, "ok")
     print(responce_form(user_id, message))
-    app.send_message(
-        user_id,
-        responce_form(message)
-    )
+    # app.send_message(
+    #     user_id,
+    #     responce_form(message)
+    # )
