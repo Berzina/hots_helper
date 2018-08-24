fa6e90e6733bc4a642e54eda79659b7d954df1f8
Ekaterina Khotko <ekhotko@plgn.ru>
2018-08-24 16:41:06 +0500

    Replace hero short profile to distinct function

--- callbacks.py
+++ callbacks.py
@@ -1,37 +1,11 @@
 def choose(app, user_id, message):
     from utils import filters
     from data.storage import BLIZZ_HEROES
-    from views import represent_stats
 
     bheroes = filters.by_choose(BLIZZ_HEROES, message)
 
-    # response = 'You can play them:\n{}'.format('\n'.join(
-    #                                            [bhero.hero.name
-    #                                             for bhero in bheroes]))
-
     for bhero in bheroes:
-        caption = '**{}**\n```{}```'.format(bhero.hero.name,
-                                            represent_stats(bhero.hero.stats))
-
-        if bhero.hero.image:
-            try:
-                app.send_photo(
-                    user_id,
-                    photo=bhero.hero.image,
-                    caption=caption
-                )
-            except Exception as e:
-                print("Can't fetch img: {}.".format(bhero.hero.image))
-                print("Reason: {}".format(e))
-                app.send_message(
-                    user_id,
-                    caption
-                )
-        else:
-            app.send_message(
-                user_id,
-                caption
-            )
+        send_hero_profile(app, user_id, bhero)
 
 
 def echo(app, user_id, message):
@@ -40,3 +14,30 @@ def echo(app, user_id, message):
         user_id,
         responce_form(user_id, message)
     )
+
+
+def send_hero_profile(app, user_id, bhero):
+    from views import represent_stats
+
+    caption = '**{}**\n```{}```'.format(bhero.hero.name,
+                                        represent_stats(bhero.hero.stats))
+
+    if bhero.hero.image:
+        try:
+            app.send_photo(
+                user_id,
+                photo=bhero.hero.image,
+                caption=caption
+            )
+        except Exception as e:
+            print("Can't fetch img: {}.".format(bhero.hero.image))
+            print("Reason: {}".format(e))
+            app.send_message(
+                user_id,
+                caption
+            )
+    else:
+        app.send_message(
+            user_id,
+            caption
+        )
